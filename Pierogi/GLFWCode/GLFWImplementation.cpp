23e8247b9e0dc9bcdc122f3b8bee1ea5226ebbff
ftrzcinka <80350380+ftrzcinka@users.noreply.github.com>
2023-12-10 20:11:54 -0500

    Update GLFWImplementation.cpp

--- Pierogi/GLFWCode/GLFWImplementation.cpp
+++ Pierogi/GLFWCode/GLFWImplementation.cpp
@@ -13,7 +13,34 @@ namespace pr {
 
 	void GLFWImplementation::CreateWindow(const std::string& name, int width, int height) {
 		mWindow = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
+
+		if (mWindow == NULL) {
+			PR_ERROR("Failed to create Window");
+			glfwTerminate();
+			return;
+		}
+
 		glfwMakeContextCurrent(mWindow);
+
+		glfwSetWindowUserPointer(mWindow, &mCallBacks);
+
+		glfwSetKeyCallback(mWindow, [](GLFWwindow* window, int keycode, int scancode, int action, int mods) {
+			if (action == GLFW_PRESS) {
+				CallBacks* callbacks{ (CallBacks*)glfwGetWindowUserPointer(window) };
+				KeyPressed e{ keycode };
+				callbacks->keyPressedFunc(e);
+			}
+			else if (action == GLFW_RELEASE) {
+				CallBacks* callbacks{ (CallBacks*)glfwGetWindowUserPointer(window) };
+				KeyReleased e{ keycode };
+				callbacks->keyReleasedFunc(e);
+			}
+			});
+
+		glfwSetWindowCloseCallback(mWindow, [](GLFWwindow* window) {
+			CallBacks* callbacks{ (CallBacks*)glfwGetWindowUserPointer(window) };
+			callbacks->windowClosedFunc();
+		});
 	}
 	int GLFWImplementation::GetHeight() const
 	{
@@ -34,5 +61,17 @@ namespace pr {
 	void GLFWImplementation::PollEvents()
 	{
 		glfwPollEvents();
+	}
+	void GLFWImplementation::SetKeyPressedCallback(std::function<void(const KeyPressed&)> callbackFunc)
+	{
+		mCallBacks.keyPressedFunc = callbackFunc;
+	}
+	void GLFWImplementation::SetKeyReleasedCallback(std::function<void(const KeyReleased&)> callbackFunc)
+	{
+		mCallBacks.keyReleasedFunc = callbackFunc;
+	}
+	void GLFWImplementation::SetWindowClosedCallback(std::function<void()> callbackFunc)
+	{
+		mCallBacks.windowClosedFunc = callbackFunc;
 	}
 }
